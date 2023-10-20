8ca1166be45d32cf86810d8b6bb4387010033c22
Devdemetrio <devdemetrio@gmail.com>
2023-10-20 18:35:24 -0400

    Iniciando o desenvolvimento do componente Game, foram adicioda apenas uma pequena estrutura do componente

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import logo from "./logo.svg";
+//CSS
+
 import "./App.css";
 
+//React
+import { useCallback, useEffect, useState } from "react";
+
+//data
+import { wordsList } from "./data/words";
+
+//components
+import StartScreen from "./componentes/StartScreen";
+import Game from "./componentes/Game";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
+
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [word] = useState(wordsList);
+  console.log(word);
   return (
     <div className="App">
-      <h2>Secreat Word</h2>
+      {gameStage === "start" && <StartScreen />}
+      {gameStage === "game" && <Game />}
     </div>
   );
 }
