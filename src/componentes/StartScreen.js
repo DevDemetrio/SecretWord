b5182aba633821f3f24db3ebc00251837a9a6a1b
Devdemetrio <devdemetrio@gmail.com>
2023-10-20 17:54:57 -0400

    Arquivo que foram salvo na branch create-style, estão sendo incorporados agora na master

---
+++ src/componentes/StartScreen.js
@@ -0,0 +1,13 @@
+import "./StartScreen.css";
+
+const StartScreen = () => {
+  return (
+    <div clasName="start">
+      <h1>Scret Word</h1>
+      <p>Clique no botao abaixo para começar a jogar</p>
+      <button>Comçar o jogo</button>
+    </div>
+  );
+};
+
+export default StartScreen;
